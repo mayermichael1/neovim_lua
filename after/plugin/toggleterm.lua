cc56ad5742ba4ab0103986565e28128ec0828b36
CodingCor <michael.mayer2105@gmail.com>
2022-11-30 17:42:12 +0100

    ToggleTerm not opening in insert mode

--- after/plugin/toggleterm.lua
+++ after/plugin/toggleterm.lua
@@ -5,20 +5,20 @@ if not status_ok then
 end
 
 toggleterm.setup({
-	size = 20,
+	size = 15,
 	open_mapping = [[<leader>t]],
 	hide_numbers = true,
 	shade_filetypes = {},
 	shade_terminals = true,
 	shading_factor = 2,
 	start_in_insert = true,
-	insert_mappings = true,
+	insert_mappings = false,
 	persist_size = true,
 	direction = "horizontal",
 	close_on_exit = true,
 	shell = vim.o.shell,
 	float_opts = {
-		border = "curved",
+		border = "single",
 		winblend = 0,
 		highlights = {
 			border = "Normal",
