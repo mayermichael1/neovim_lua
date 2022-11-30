9bebd5e99a2a672771a781f539554ad822f0bd1a
CodingCor <michael.mayer2105@gmail.com>
2022-11-30 11:49:31 +0100

    Removed Neogit from Plugins

--- after/plugin/nvim-tree.lua
+++ after/plugin/nvim-tree.lua
@@ -7,5 +7,61 @@ end
 vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<cr>", {silent = true, noremap = true})
 
 nvimtree.setup({
-
+    renderer = {
+        add_trailing = false,
+        group_empty = false,
+        highlight_git = false,
+        full_name = false,
+        highlight_opened_files = "none",
+        indent_width = 2,
+        indent_markers = {
+          enable = false,
+          inline_arrows = true,
+          icons = {
+            corner = "└",
+            edge = "│",
+            item = "│",
+            bottom = "─",
+            none = " ",
+          },
+        },
+        icons = {
+          webdev_colors = true,
+          git_placement = "before",
+          padding = " ",
+          symlink_arrow = " ➛ ",
+          show = {
+            file = true,
+            folder = true,
+            folder_arrow = true,
+            git = true,
+          },
+          glyphs = {
+            default = "",
+            symlink = "",
+            bookmark = "",
+            folder = {
+              arrow_closed = "",
+              arrow_open = "",
+              default = "",
+              open = "",
+              empty = "",
+              empty_open = "",
+              symlink = "",
+              symlink_open = "",
+            },
+            git = {
+              unstaged = "X",
+              staged = "S",
+              unmerged = "M",
+              renamed = "➜",
+              untracked = "U",
+              deleted = "-",
+              ignored = "◌",
+            },
+          },
+        },
+        special_files = { "Cargo.toml", "Makefile", "README.md", "readme.md" },
+        symlink_destination = true,
+    },
 })
--- lua/codingcor/packer.lua
+++ lua/codingcor/packer.lua
@@ -40,7 +40,6 @@ return packer.startup(function(use)
     use 'nvim-treesitter/playground'
 
     -- git integration
-    use { 'TimUntersberger/neogit', requires = 'nvim-lua/plenary.nvim' }
     use "lewis6991/gitsigns.nvim"
     
     -- telescope
