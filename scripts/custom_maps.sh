089e8390bf76d09d4417dad66d15d4a05bede90a
CodingCor <michael.mayer2105@gmail.com>
2022-11-30 19:35:53 +0100

    Added Keymap Script

--- scripts/custom_maps.sh
+++ scripts/custom_maps.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-echo "*custom_maps.txt* Custom Keymaps in Neovim Config" > $HOME/.config/nvim/doc/custom_maps.txt
-echo "*custom_maps* " >> $HOME/.config/nvim/doc/custom_maps.txt
+MAPS="$HOME/.config/nvim/doc/custom_maps.txt"
 
-grep -Rhs "vim.keymap.set" $HOME/.config/nvim | awk '{$1=$1;print}' | sed 's/^/ - /' >> $HOME/.config/nvim/doc/custom_maps.txt
+
+echo "*custom_maps.txt* Custom Keymaps in Neovim Config" > $MAPS
+echo "*custom_maps* " >> $MAPS
+echo "" >> $MAPS
+echo "Keymappings: " >> $MAPS
+
+grep -Rhs "vim.keymap.set" $HOME/.config/nvim --exclude-dir=scripts | awk '{$1=$1;print}' | sed 's/^/ - /' >> $MAPS
