bc3264e94f0bb1d4d3438f27d93adad8fec47ecb
CodingCor <michael.mayer2105@gmail.com>
2022-12-01 21:07:14 +0100

    LSP added go to definition

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -13,6 +13,7 @@ vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
 local on_attach = function(client, bufnr)
     local bufopts = {noremap=true, silent=true,buffer=bufnr}
     vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
     vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
     vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
     vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
