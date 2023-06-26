beff26ab721e141ec9241d664d16dc1f8815c999
CodingCor <michael.mayer2105@gmail.com>
2023-06-26 20:49:18 +0200

    Removed build commands for cpp make will be used instead of cmake for now

--- after/ftplugin/cpp.lua
+++ after/ftplugin/cpp.lua
@@ -1,2 +1,2 @@
-vim.api.nvim_buf_create_user_command(0, 'CMake', '!cmake -B %:p:h/build;', {});
-vim.api.nvim_buf_create_user_command(0, 'Compile', '!cmake --build %:p:h/build;', {});
+-- vim.api.nvim_buf_create_user_command(0, 'CMake', '!cmake -B %:p:h/build;', {});
+-- vim.api.nvim_buf_create_user_command(0, 'Compile', '!cmake --build %:p:h/build;', {});
