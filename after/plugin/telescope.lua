e60e16bbd5aacf09f049c801b601a1454a7343a4
CodingCor <michael.mayer2105@gmail.com>
2024-03-05 20:52:27 +0100

    Added Telescope binding to search in open buffers

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,3 +1,8 @@
+function grep_in_buffer()
+    local live_grep  = require("telescope.builtin");
+    live_grep.live_grep({grep_open_files=true});
+end
+
 local status_ok, telescope = pcall(require, "telescope")
 if not status_ok then
     print("Telescope could not be loaded")
@@ -25,3 +30,5 @@ local opts = {noremap = true, silent = true}
 vim.keymap.set("n", "<leader>ff", ":Telescope find_files<cr>", opts) -- search files
 vim.keymap.set("n", "<leader>fb", ":Telescope buffers<cr>", opts) -- search buffers
 vim.keymap.set("n", "<leader>fg", ":Telescope live_grep<cr>", opts) -- search with grep
+vim.keymap.set("n", "<leader>fo", grep_in_buffer, opts); -- search in open buffers
+
