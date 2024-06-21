85ce5a254dc5cd1a995c6d600729391aed2b6470
mayermichael1 <michael.mayer2105@gmail.com>
2024-06-21 18:38:15 +0200

    change colorscheme to different style

--- after/plugin/colorscheme.lua
+++ after/plugin/colorscheme.lua
@@ -1,3 +1,5 @@
-vim.g.sonokai_transparent_background = 2
-vim.g.sonokai_style = 'espresso'
+--vim.g.sonokai_transparent_background = 0
+vim.g.sonokai_style = 'shusia'
+vim.g.sonokai_better_performance = 1;
+vim.g.sonokai_disable_italic_comment = 1;
 vim.cmd("colorscheme sonokai")
