363bfbfd12002d9b50cbcab10421d7d21d2390f5
mayermichael1 <michael.mayer2105@gmail.com>
2024-06-21 20:46:14 +0200

    removed a bunch of window related keymaps

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -18,24 +18,24 @@ vim.g.maplocalleader = " "
 -- normal mode remaps
 
 -- window navigation
-vim.keymap.set("n", "<C-h>", "<C-w>h", opts) -- move to the left window
-vim.keymap.set("n", "<C-j>", "<C-w>j", opts) -- move to the bottom window
-vim.keymap.set("n", "<C-k>", "<C-w>k", opts) -- move to the top window
-vim.keymap.set("n", "<C-l>", "<C-w>l", opts) -- move to the right window
+--vim.keymap.set("n", "<C-h>", "<C-w>h", opts) -- move to the left window
+--vim.keymap.set("n", "<C-j>", "<C-w>j", opts) -- move to the bottom window
+--vim.keymap.set("n", "<C-k>", "<C-w>k", opts) -- move to the top window
+--vim.keymap.set("n", "<C-l>", "<C-w>l", opts) -- move to the right window
 
 -- resize windows
-vim.keymap.set("n", "<C-Up>", ":resize +2<cr>", opts) -- resize vertial +
-vim.keymap.set("n", "<C-Down>", ":resize -2<cr>", opts) -- resize vertical -
-vim.keymap.set("n", "<C-Left>", ":vertical resize -2<cr>", opts) -- resize horizontal -
-vim.keymap.set("n", "<C-Rigth>", ":vertical resize +2<cr>", opts) -- resize horizontal +
+--vim.keymap.set("n", "<C-Up>", ":resize +2<cr>", opts) -- resize vertial +
+--vim.keymap.set("n", "<C-Down>", ":resize -2<cr>", opts) -- resize vertical -
+--vim.keymap.set("n", "<C-Left>", ":vertical resize -2<cr>", opts) -- resize horizontal -
+--vim.keymap.set("n", "<C-Rigth>", ":vertical resize +2<cr>", opts) -- resize horizontal +
 
 -- navigate buffers
 vim.keymap.set("n", "<S-l>", ":bnext<cr>", opts) -- next buffer
 vim.keymap.set("n", "<S-h>", ":bprevious<cr>", opts) -- previous buffer
 
 -- insert mode
-vim.keymap.set("i", "jk", "<ESC>", opts) -- exist insert mode
-
+--vim.keymap.set("i", "jk", "<ESC>", opts) -- exist insert mode
+   
 -- visual mode
 vim.keymap.set("v", "<", "<gv", opts) -- reselect after indentation
 vim.keymap.set("v", ">", ">gv", opts) -- reselect after indentation
