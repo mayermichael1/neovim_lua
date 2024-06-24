03a12bdf925302c38367049914f85e0c6329c80f
CodingCor <michael.mayer2105@gmail.com>
2024-06-24 23:22:23 +0200

    tapstop 2 for cpp files

--- after/ftplugin/cpp.lua
+++ after/ftplugin/cpp.lua
@@ -1,2 +1,5 @@
 -- vim.api.nvim_buf_create_user_command(0, 'CMake', '!cmake -B %:p:h/build;', {});
 -- vim.api.nvim_buf_create_user_command(0, 'Compile', '!cmake --build %:p:h/build;', {});
+vim.opt_local.tabstop = 2
+vim.opt_local.softtabstop = 2
+vim.opt_local.shiftwidth = 2
