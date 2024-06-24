f85a685d5b04e543a245d791c9075edd99e8ffae
CodingCor <michael.mayer2105@gmail.com>
2024-06-24 23:27:09 +0200

    default lualine config

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -4,20 +4,22 @@ if not status_ok then
     return
 end
 
-lualine.setup{
-    options = {
-        theme = "auto",
-        component_separators = { left = '', right = ''},
-        section_separators = { left = '', right = ''},
-        globalstatus = true,
-    },
-    tabline = {
-        lualine_a = {'buffers'},
-        lualine_b = {},
-        lualine_c = {},
-        lualine_x = {},
-        lualine_y = {},
-        lualine_z = {'tabs'},
-    },
-}
+--lualine.setup{
+--    options = {
+--        theme = "auto",
+--        component_separators = { left = '', right = ''},
+--        section_separators = { left = '', right = ''},
+--        globalstatus = true,
+--    },
+--    tabline = {
+--        lualine_a = {'buffers'},
+--        lualine_b = {},
+--        lualine_c = {},
+--        lualine_x = {},
+--        lualine_y = {},
+--        lualine_z = {'tabs'},
+--    },
+--}
+--
+lualine.setup()
 
