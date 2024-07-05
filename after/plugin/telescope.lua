d1502f062051157328f55339ece44c1b608e095b
CodingCor <michael.mayer2105@gmail.com>
2024-07-05 17:12:37 +0200

    Update Telescope config

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,34 +1,19 @@
-function grep_in_buffer()
-    local live_grep  = require("telescope.builtin");
-    live_grep.live_grep({grep_open_files=true});
-end
-
 local status_ok, telescope = pcall(require, "telescope")
 if not status_ok then
     print("Telescope could not be loaded")
     return
 end
 
-telescope.setup{
-    defaults = {
-        theme = "ivy"
-    },
-    pickers = {
-        find_files = {
-            theme = "ivy"
-        },
-        buffers = {
-            theme = "ivy"
-        },
-        live_grep = {
-            theme = "ivy"
-        }
-    }
-}
+telescope.setup{}
 
 local opts = {noremap = true, silent = true}
 vim.keymap.set("n", "<leader>ff", ":Telescope find_files<cr>", opts) -- search files
 vim.keymap.set("n", "<leader>fb", ":Telescope buffers<cr>", opts) -- search buffers
 vim.keymap.set("n", "<leader>fg", ":Telescope live_grep<cr>", opts) -- search with grep
-vim.keymap.set("n", "<leader>fo", grep_in_buffer, opts); -- search in open buffers
+
+--vim.keymap.set("n", "<leader>fo", grep_in_buffer, opts); -- search in open buffers
+--function grep_in_buffer()
+--    local live_grep  = require("telescope.builtin");
+--    live_grep.live_grep({grep_open_files=true});
+--end
 
