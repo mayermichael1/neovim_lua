f234f7c475cd0e10267125b8fbb1a7f790e9e006
CodingCor <michael.mayer2105@gmail.com>
2024-07-11 23:24:51 +0200

    improve snippet keypoints

--- after/plugin/luasnip.lua
+++ after/plugin/luasnip.lua
@@ -5,14 +5,14 @@ if not snip_status_ok then
 end
 
 vim.keymap.set({"i"}, "<C-K>", function() luasnip.expand() end, {silent = true})
-vim.keymap.set({"i", "s"}, "<C-L>", function() luasnip.jump( 1) end, {silent = true})
-vim.keymap.set({"i", "s"}, "<C-J>", function() luasnip.jump(-1) end, {silent = true})
+vim.keymap.set({"i", "s"}, "<C-J>", function() luasnip.jump( 1) end, {silent = true})
+vim.keymap.set({"i", "s"}, "<C-K>", function() luasnip.jump(-1) end, {silent = true})
 
-vim.keymap.set({"i", "s"}, "<C-E>", function()
-	if luasnip.choice_active() then
-		luasnip.change_choice(1)
-	end
-end, {silent = true})
+--vim.keymap.set({"i", "s"}, "<C-E>", function()
+--	if luasnip.choice_active() then
+--		luasnip.change_choice(1)
+--	end
+--end, {silent = true})
 
 require("luasnip.loaders.from_snipmate").load();
 -- this loads snippets in snipmate format from snippets directory
