a3f2ca4646fd53d630255a0e281d2ae6918465ce
mayermichael1 <michael.mayer2105@gmail.com>
2025-07-17 22:49:58 +0200

    fixing snippet keymap

--- after/plugin/luasnip.lua
+++ after/plugin/luasnip.lua
@@ -6,7 +6,7 @@ end
 
 vim.keymap.set({"i"}, "<C-K>", function() luasnip.expand() end, {silent = true})
 vim.keymap.set({"i", "s"}, "<C-J>", function() luasnip.jump( 1) end, {silent = true})
-vim.keymap.set({"i", "s"}, "<C-K>", function() luasnip.jump(-1) end, {silent = true})
+vim.keymap.set({"i", "s"}, "<C-L>", function() luasnip.jump(-1) end, {silent = true})
 
 --vim.keymap.set({"i", "s"}, "<C-E>", function()
 --	if luasnip.choice_active() then
