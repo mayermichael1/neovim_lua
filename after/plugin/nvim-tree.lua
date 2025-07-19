ab19d79773a9c3340395291710855beef44c0f11
mayermichael1 <michael.mayer2105@gmail.com>
2025-07-19 13:12:08 +0200

    remove unneeded nvim-tree

--- after/plugin/nvim-tree.lua
+++
@@ -1,9 +0,0 @@
-local status_ok, nvimtree = pcall(require, "nvim-tree")
-if not status_ok then
-    print("Nvim-Tree could not be loaded")
-    return
-end
-
---vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<cr>", {silent = true, noremap = true}) -- show file explorer
-
---nvimtree.setup({})
