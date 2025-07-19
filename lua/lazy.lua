8197cab5ddb569c10f2afa073fd59f2a17858afa
mayermichael1 <michael.mayer2105@gmail.com>
2025-07-19 17:09:33 +0200

    move cmp config to lazy

--- lua/lazy.lua
+++ lua/lazy.lua
@@ -26,12 +26,41 @@ lazy.setup({
     {'luisiacc/gruvbox-baby', lazy = true},
 
     -- cmp plugins
-    "hrsh7th/nvim-cmp", -- The completion plugin
-    "hrsh7th/cmp-buffer", -- buffer completions
-    "hrsh7th/cmp-path", -- path completions
-    "hrsh7th/cmp-cmdline", -- cmdline completions
-    "hrsh7th/cmp-nvim-lsp", -- lsp source for cmp
-    "hrsh7th/cmp-nvim-lua", -- lua source for cmp
+    {"hrsh7th/nvim-cmp", 
+        dependencies = {
+            "hrsh7th/cmp-buffer", -- buffer completions
+            "hrsh7th/cmp-path", -- path completions
+            "hrsh7th/cmp-cmdline", -- cmdline completions
+            "hrsh7th/cmp-nvim-lsp", -- lsp source for cmp
+            "hrsh7th/cmp-nvim-lua", -- lua source for cmp
+            "saadparwaiz1/cmp_luasnip", -- snippet completions-- completion
+        },
+        config = function()
+            local cmp = require("cmp");
+            cmp.setup({
+                snippet = {
+                    expand = function(args)
+                        require("luasnip").lsp_expand(args.body) -- use luasnip as snipping engine
+                    end,
+                },
+                mapping = cmp.mapping.preset.insert({
+                    ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+                    ['<C-d>'] = cmp.mapping.scroll_docs(4),
+                    ['<C-Space>'] = cmp.mapping.complete(),
+                    ['<C-e>'] = cmp.mapping.abort(),
+                    ['<CR>'] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+                }),
+                sources = {
+                    { name = "nvim_lsp" },
+                    { name = "nvim_lua" },
+                    { name = "luasnip" },
+                    { name = "buffer" },
+                    { name = "path" },
+                },
+            })
+        end
+        
+    }, 
 
     -- snipping engine
     {"L3MON4D3/LuaSnip",
@@ -55,7 +84,6 @@ lazy.setup({
             -- require("luasnip.loaders.from_vscode").lazy_load({ paths = { "./my-cool-snippets" } })
         end
     }, --snippet engine
-    "saadparwaiz1/cmp_luasnip", -- snippet completions-- completion
 
     -- lsp 
     "neovim/nvim-lspconfig", -- enable LSP
