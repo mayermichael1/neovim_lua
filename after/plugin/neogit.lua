7f574200d49c39a4179a3c421c404425fbc40468
mayermichael1 <michael.mayer2105@gmail.com>
2025-07-19 13:12:36 +0200

    remove empty configs

--- after/plugin/neogit.lua
+++
@@ -1,7 +0,0 @@
-local status_ok, neogit = pcall(require, "neogit")
-if not status_ok then
-    print("Neogit could not be loaded")
-    return
-end
-
-neogit.setup {}
--- after/plugin/signs.lua
+++
@@ -1,7 +0,0 @@
-local status_ok, gitsigns = pcall(require, "gitsigns")
-if not status_ok then
-    print("Gitsigns could not be loaded")
-    return
-end
-
-gitsigns.setup{}
