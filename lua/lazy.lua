3c8cd6ab07563635d7e2bef93869f1ff0f104ed4
mayermichael1 <michael.mayer2105@gmail.com>
2025-07-19 13:27:46 +0200

    pull in treesitter config

--- lua/lazy.lua
+++ lua/lazy.lua
@@ -41,7 +41,30 @@ lazy.setup({
     "neovim/nvim-lspconfig", -- enable LSP
 
     -- treesitter
-    {'nvim-treesitter/nvim-treesitter', build = ':TSUpdate'},
+    {'nvim-treesitter/nvim-treesitter', 
+        build = ':TSUpdate',
+        opts = {
+            ensure_installed = { "" },
+            sync_install = false,
+            auto_install = false,
+            ignore_install = { "" },
+            highlight = {
+                enable = true,
+                disable = function(lang, buf)
+                    local max_filesize = 100 * 1024 -- 100 KB
+                    local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+                    if ok and stats and stats.size > max_filesize then
+                        return true
+                    end
+                end,
+                additional_vim_regex_highlighting = false,
+            },
+            indent = {
+                enable = false,
+                disable = {"dart"},
+            }
+        }
+    },
     {'nvim-treesitter/nvim-treesitter-context', lazy = true},
     {'nvim-treesitter/playground', lazy = true},
 
