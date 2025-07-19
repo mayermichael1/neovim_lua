cbffd35588893d4283899629caa30a0d0ad35b99
mayermichael1 <michael.mayer2105@gmail.com>
2025-07-19 13:21:15 +0200

    show tabs again

--- lua/lazy.lua
+++ lua/lazy.lua
@@ -75,12 +75,7 @@ lazy.setup({
                 globalstatus = true,
             },
             tabline = {
-                lualine_a = {'buffers'},
-                lualine_b = {},
-                lualine_c = {},
-                lualine_x = {},
-                lualine_y = {},
-                lualine_z = {'tabs'},
+                lualine_a = {'tabs'},
             },
         }
     },
