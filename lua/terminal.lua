06c1d2cabe79c30fa4ff91cb26d1cbc5905e0377
mayermichael1 <michael.mayer2105@gmail.com>
2025-08-03 16:16:15 +0200

    directly build using <leader>tb

--- lua/terminal.lua
+++ lua/terminal.lua
@@ -9,18 +9,23 @@ vim.api.nvim_create_autocmd('TermOpen',
 
 function open_term()
     vim.cmd("tab term")
+    run_in_terminal("clear");
 end
 
-vim.keymap.set("n", "<leader>rt", function()
+function run_in_terminal(command)
     if (term_id == 0)
     then
         open_term()
     end
+    local complete_command = string.format("%s \r\n", command)
+    vim.fn.chansend(term_id, {complete_command})
+end
 
-    if (term_id ~= 0) 
-    then
-        local command = vim.fn.input("Command to run in terminal: ")
-        local complete_command = string.format("%s \r\n", command)
-        vim.fn.chansend(term_id, {complete_command})
-    end
+vim.keymap.set("n", "<leader>tr", function(command)
+    command = vim.fn.input("Command to run in terminal: ")
+    run_in_terminal(command);
+end)
+
+vim.keymap.set("n", "<leader>tb", function(command)
+    run_in_terminal("bake");
 end)
