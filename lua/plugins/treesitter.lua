2e692b648fd05bcfd77ab521db3cb8dcd7bfd496
mayermichael1 <michael.mayer2105@gmail.com>
2025-08-24 16:53:59 +0200

    make treesitter auto start again

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,27 +1,32 @@
 return {
     {'nvim-treesitter/nvim-treesitter', 
+        lazy = false,
         build = ':TSUpdate',
-        opts = {
-            ensure_installed = { "" },
-            sync_install = false,
-            auto_install = false,
-            ignore_install = { "" },
-            highlight = {
-                enable = true,
-                disable = function(lang, buf)
-                    local max_filesize = 100 * 1024 -- 100 KB
-                    local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-                    if ok and stats and stats.size > max_filesize then
-                        return true
-                    end
-                end,
-                additional_vim_regex_highlighting = false,
-            },
-            indent = {
-                enable = false,
-                disable = {"dart"},
-            }
-        }
+        config = function()
+            local configs = require 'nvim-treesitter.configs'
+            configs.setup({
+
+                ensure_installed = { "" },
+                sync_install = false,
+                auto_install = false,
+                ignore_install = { "" },
+                highlight = {
+                    enable = true,
+                    disable = function(lang, buf)
+                        local max_filesize = 100 * 1024 -- 100 KB
+                        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+                        if ok and stats and stats.size > max_filesize then
+                            return true
+                        end
+                    end,
+                    additional_vim_regex_highlighting = false,
+                },
+                indent = {
+                    enable = false,
+                    disable = {"dart"},
+                }
+            })
+        end
     },
     {'nvim-treesitter/nvim-treesitter-context', lazy = true},
     {'nvim-treesitter/playground', lazy = true}
