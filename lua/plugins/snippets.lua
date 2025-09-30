e0a367e4e3318d138abd363e44b1491b0129abb1
mayermichael1 <michael.mayer2105@gmail.com>
2025-09-30 17:32:25 +0200

    change snippet keymaps

--- lua/plugins/snippets.lua
+++ lua/plugins/snippets.lua
@@ -6,7 +6,7 @@ return {
         init = function()
             local luasnip = require("luasnip");
             vim.keymap.set({"i"}, "<C-K>", function() luasnip.expand() end, {silent = true})
-            vim.keymap.set({"i", "s"}, "<C-J>", function() luasnip.jump( 1) end, {silent = true})
+            vim.keymap.set({"i", "s"}, "<C-H>", function() luasnip.jump( 1) end, {silent = true})
             vim.keymap.set({"i", "s"}, "<C-L>", function() luasnip.jump(-1) end, {silent = true})
 
             --vim.keymap.set({"i", "s"}, "<C-E>", function()
