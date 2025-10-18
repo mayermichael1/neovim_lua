ad80982a277ccc46008bc0a97dfac6685c93099c
mayermichael1 <michael.mayer2105@gmail.com>
2025-10-18 16:16:33 +0200

    active snippet shortcuts again

--- lua/plugins/snippets.lua
+++ lua/plugins/snippets.lua
@@ -5,9 +5,9 @@ return {
         version = "v2.*",
         init = function()
             local luasnip = require("luasnip");
-            --vim.keymap.set({"i"}, "<C-K>", function() luasnip.expand() end, {silent = true})
-            --vim.keymap.set({"i", "s"}, "<C-H>", function() luasnip.jump( 1) end, {silent = true})
-            --vim.keymap.set({"i", "s"}, "<C-L>", function() luasnip.jump(-1) end, {silent = true})
+            vim.keymap.set({"i"}, "<C-K>", function() luasnip.expand() end, {silent = true})
+            vim.keymap.set({"i", "s"}, "<C-H>", function() luasnip.jump(-1) end, {silent = true})
+            vim.keymap.set({"i", "s"}, "<C-L>", function() luasnip.jump( 1) end, {silent = true})
 
             --vim.keymap.set({"i", "s"}, "<C-E>", function()
             --	if luasnip.choice_active() then
